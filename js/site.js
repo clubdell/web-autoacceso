d5c1d3de72a6304304ef0d211e59db7365d032a9
Antonio Campos <geckocore03@outlook.com>
2018-09-01 19:03:30 -0500

    Some changes, added slider

--- js/site.js
+++ js/site.js
@@ -8,4 +8,46 @@
       navbar.addClass("navbar-scroll");
     }
   });
+
+  var slickSliders = function() {
+    $('.multiple-items').slick({
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      dots: true,
+      infinite: true,
+
+      autoplay: true,
+      autoplaySpeed: 2000,
+
+      arrows: true,
+      nextArrow: '<span class="next"><i class="fas fa-angle-right"></i></span>',
+      prevArrow: '<span class="prev"><i class="fas fa-angle-left"></i></span>',
+      draggable: false,
+      responsive: [{
+        breakpoint: 1125,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 580,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]});
+  };
+
+  slickSliders();
 })(jQuery);
